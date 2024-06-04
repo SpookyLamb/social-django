15ea3227a3dc57ca6e777de908fbf5eae8ad6a42
SpookyLamb <turtleytyrant@gmail.com>
2024-06-04 09:08:11 -0400

    URL update.

--- social_django/urls.py
+++ social_django/urls.py
@@ -29,6 +29,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create-user/', create_user),
     path('profile/', get_profile),
     path('refresh/', TokenRefreshView.as_view()),
     path('token/', TokenObtainPairView.as_view()),
